987bafda3fbef6ddeda0df6e460e95ff4be39678
Abdelrahman-Hussien-7200 <abdhus5472@gmail.com>
2023-10-23 06:26:40 -0700

    abdo

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,72 +1,76 @@
 #include "sort.h"
 
 /**
- * swap - swap two int
- * @a: int
- * @b: int
- * Return: (void) Swaped int
+ * swap - swap two integers numbers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: void
  */
+
 void swap(int *a, int *b)
 {
-	int tmp;
+	int temp;
 
-	tmp = *a;
+	temp = *a;
 	*a = *b;
-	*b = tmp;
+	*b = temp;
 }
 /**
- * partition - Partition an array and using pivot
- * @array: Array
- * @low: int
- * @high: int
+ * party - array and using pivot
+ * @array: the Array
+ * @low_n: integer
+ * @high_n: integer
  * @size: size of array (size_t)
  * Return: index of pivote (int)
- */
-int partition(int *array, int low, int high, size_t size)
-{
-	int pivot = array[high];
-	int x = low - 1, y;
+*/
 
-	for (y = low; y <= high; y++)
+int party(int *array, int low_n, int high_n, size_t size)
+{
+	int pivot = array[high_n];
+	int i = low_n - 1, j;
+
+	for (j = low_n; j <= high_n; j++)
 	{
-		if (array[y] <= pivot)
+		if (array[j] <= pivot)
 		{
-			x++;
-			if (x != y)
+			i++;
+			if (i != j)
 			{
-				swap(&array[x], &array[y]);
+				swap(&array[i], &array[j]);
 				print_array(array, size);
 			}
 		}
 	}
-	return (x);
+	return (i);
 }
-/**
- * lomuto_qsort - Sorting Recursively an Array
- * @array: Array to be sorted
- * @low: The lowest value of the array
- * @high: highest value of the array
- * @size: Size of The Array
- * Return: void
- */
-void lomuto_qsort(int *array, int low, int high, size_t size)
-{
-	int i;
 
-	if (low < high)
+/**
+ * Lomuto_partition - Sorting Recursively
+ * @array: Array to be sorted
+ * @low_n: lowest value
+ * @high_n: highest value
+ * @size: Array size
+ * Return: void
+*/
+
+void Lomuto_partition(int *array, int low_n, int high_n, size_t size)
+{
+	int n;
+
+	if (low_n < high_n)
 	{
-		i = partition(array, low, high, size);
-		lomuto_qsort(array, low, i - 1, size);
-		lomuto_qsort(array, i + 1, high, size);
+		n = party(array, low_n, high_n, size);
+		Lomuto_partition(array, low_n, n - 1, size);
+		Lomuto_partition(array, n + 1, high_n, size);
 	}
 }
 /**
- * quick_sort - Quick Sort Algorithme using lomuto partition
- * @array: Array to sort
- * @size: Size of The Array
- * Return: Sorted Array (void)
+ * quick_sort - Quick Sort
+ * @array: Array to be sorted
+ * @size: Array size
+ * Return: void
  */
 void quick_sort(int *array, size_t size)
 {
-	lomuto_qsort(array, 0, size - 1, size);
+	Lomuto_partition(array, 0, size - 1, size);
 }
